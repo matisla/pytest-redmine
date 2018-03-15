bc4d67998c8d80ace232b9854b3225cd280f1543
Matthieu Herrmann <herrmann.matthie@gmail.com>
2018-03-15 01:07:40 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding UTF-8
+
+from setuptools import setup
+from setuptools import find_packages
+
+def readme():
+    with open('README.md', 'r') as readme_file:
+        return readme_file.read()
+
+
+setup(
+    name='pytest-redmine',
+    version='0.0.1',
+    license='MIT',
+    description='Pytest plugin for redmine',
+    long_description= readme(),
+    url='https://github.com/matisla/pytest-redmine',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'License :: OSI :: MIT License',
+        'Operation System :: Microsoft :: Windows',
+        'Operation System :: POSIX',
+        'Topic :: Software Developpement :: Testing',
+        'Topic :: Utilities',
+    ],
+    keywords=[
+        'redmine', 'pytest', 'py.test'
+    ],
+    install_requires=[
+        'pytest',
+    ],
+    python_requires='>=3.6',
+)
+        
+
